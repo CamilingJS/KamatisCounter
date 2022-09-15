9f0165271a9fda1f51f8266dad83b2016ff459ea
CamilingJS <jonathancamiling@gmail.com>
2022-09-14 23:30:19 -0700

    attempting to change day text to red when box lateral to it is clicked

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import { useState } from "react";
 
 /*
   1. Select day, Turn it bold (default "M")
@@ -10,14 +11,21 @@ import "./styles.css";
 const days = ["M", "T", "W", "Th", "F", "Sa", "Su"];
 
 export default function App() {
+
+  const [textColor, setTextColor] = useState("grey")
+
+  const toggleTextColor = (e) => {
+    setTextColor(textColor === 'grey' ? '#800000' : 'grey')
+  }
+
   return (
     <div className="App">
       <div className="window">
-        <h2>Tomato Counter</h2>
+        <h2>Kamatis Counter</h2>
         {days.map((day) => (
           <div key={day} className="tomato-box">
-            <h3>{day}</h3>
-            <div className="tomato-day-box"></div>
+            <h3 style={{color:textColor}}>{day}</h3>
+            <div onClick={toggleTextColor}  className="tomato-day-box"></div>
           </div>
         ))}
         <div className="buttons-container">
