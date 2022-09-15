0cd8e3cdd4abf6453fa4c139f92b9d2e93fec93d
CamilingJS <jonathancamiling@gmail.com>
2022-09-14 23:54:37 -0700

    1. utilizing useState hook for selecteDay 2. in the onClick setSelectedDay param is day

--- src/App.js
+++ src/App.js
@@ -12,20 +12,18 @@ const days = ["M", "T", "W", "Th", "F", "Sa", "Su"];
 
 export default function App() {
 
-  const [textColor, setTextColor] = useState("grey")
+  const [selectedDay, setSelectedDay] = useState("M")
 
-  const toggleTextColor = (e) => {
-    setTextColor(textColor === 'grey' ? '#800000' : 'grey')
-  }
+  console.log(selectedDay, "is the day")
 
   return (
     <div className="App">
       <div className="window">
         <h2>Kamatis Counter</h2>
         {days.map((day) => (
-          <div key={day} className="tomato-box">
-            <h3 style={{color:textColor}}>{day}</h3>
-            <div onClick={toggleTextColor}  className="tomato-day-box"></div>
+          <div onClick={()=>{setSelectedDay(day)}} key={day} className="tomato-box">
+            <h3 >{day}</h3>
+            <div  className="tomato-day-box"></div>
           </div>
         ))}
         <div className="buttons-container">
